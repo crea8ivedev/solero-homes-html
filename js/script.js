b35aa083545e662a853a2ff318f66680c503e2aa
Crea8ive Dev <crea8ivedev@gmail.com>
2025-09-17 17:31:38 +0530

    Design changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
+// Header sticky
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('.header');
+  if (window.scrollY > 100) { // Adjust value as needed
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
+
 document.addEventListener('DOMContentLoaded', function () {
   // Services
   var swiper = new Swiper('.service-slider', {
     loop: true,
     slidesPerView: 1,
+    autoplay: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -13,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var swiper = new Swiper('.review-slider', {
     loop: true,
     slidesPerView: 1,
+    autoplay: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -30,5 +42,25 @@ document.addEventListener('DOMContentLoaded', function () {
     if (linkPage === currentPage) {
       link.classList.add("active");
     }
+  });
+
+  // Video play & pause
+  const video = document.getElementById("video");
+  const circlePlayButton = document.getElementById("circle-play-b");
+
+  function togglePlay() {
+    if (video.paused || video.ended) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+
+  circlePlayButton.addEventListener("click", togglePlay);
+  video.addEventListener("playing", function () {
+    circlePlayButton.style.opacity = 0;
+  });
+  video.addEventListener("pause", function () {
+    circlePlayButton.style.opacity = 1;
   });
 });
